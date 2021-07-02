0006cfc8f0ed47618deeb6e4c640c6ad9695bad6
Jonathan Quintero Giraldo <jonathan.quintero@newshore.es>
2021-07-01 19:34:11 -0500

    resolve comments pr

--- server/index.js
+++ server/index.js
@@ -3,5 +3,6 @@ const app = express()
 
 const PORT = 3000
 
-
-app.listen(PORT, () => console.log(`Listenig on port ${PORT}`))
+app.listen(PORT, () => {
+    console.log(`Listenig on port ${PORT}`)    
+})
